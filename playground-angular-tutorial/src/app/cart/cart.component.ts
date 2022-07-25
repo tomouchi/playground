bb71222ff965daf5b299eb5e20a6ebdb672af322
tomouchi <29200586+tomouchi@users.noreply.github.com>
2022-07-25 22:40:40 +0900

    cart view
    - カートのアイテムを表示する

--- playground-angular-tutorial/src/app/cart/cart.component.ts
+++ playground-angular-tutorial/src/app/cart/cart.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
+import { CartService } from "../cart.service";
 
 @Component({
-  selector: 'app-cart',
-  templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  selector: "app-cart",
+  templateUrl: "./cart.component.html",
+  styleUrls: ["./cart.component.css"],
 })
-export class CartComponent implements OnInit {
+export class CartComponent {
+  items = this.cartService.getItems();
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor(private cartService: CartService) {}
 }
